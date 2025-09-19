ce5c0db03f9a7b34dd84b919429fdfb2d34296c9
Astark041304 <163118272+Astark041304@users.noreply.github.com>
2025-09-19 21:16:36 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,19 @@
 <?php
 
+$conn = mysqli_connect (
+   getenv("DB_HOST"),
+   getenv("DB_USERNAME"),
+   getenv("DB_PASSWORD'),
+   getenv("DB_DATABASE"),
+   getenv("DB_PORT") ?: '3306'
+);
+
+if (!$conn) {
+     die("Connection failed: " . mysqli_connect_error());
+}
+
+                        )
+
 $servername = "localhost"; 
 $username = "root"; 
 $password = ""; 
